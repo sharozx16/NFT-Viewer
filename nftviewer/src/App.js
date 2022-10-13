ade5180e9031e7e24a442fa2881a2e16751ff093
sharozx16 <sharoz16@gmail.com>
2022-10-13 15:00:34 +0500

    viewedbyall

--- nftviewer/src/App.js
+++ nftviewer/src/App.js
@@ -1,19 +1,21 @@
 
 import contractABI from "./contract.json";
 import "./App.css";
-import { ethers } from "ethers";
+import { ethers, Wallet } from "ethers";
 import { useState, useEffect } from "react";
 
 
 function App() {
   const [state, setState] = useState(true);
   const [token, setToken] = useState([]);
- const[account,setAccount] = useState()
 
-  const connectContract = async (signer) => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0])
-    console.log("accounts:",accounts[0])
+
+  const connectContract = async (provider) => {
+
+    const privateKey = "bff39cb7c9e44a6ecf4f4d7a143fbb3c934576ff67cf784c6a190a980e6abc19";
+    const signer = new ethers.Wallet(privateKey,provider)
+  
+  
     const address = "0xC2984F58901a1cECAde22d8be4aA07e2Ee67f28d";
     const contract = new ethers.Contract(address, contractABI, signer);
     getMetaData(contract);
@@ -38,10 +40,7 @@ function App() {
 
   useEffect(() => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-
-    const signer = provider.getSigner()   
-    console.log("Signer : ",signer)
-    connectContract(signer);
+    connectContract(provider);
   }, []);
 
   return (
